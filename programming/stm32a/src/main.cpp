a23c81ebecb6e12e74d39b6827167609ec1fa271
neohaojun <neohaojun@gmail.com>
2023-04-14 01:36:48 +0800

    mostly complete light detection program

--- programming/stm32a/src/main.cpp
+++ programming/stm32a/src/main.cpp
@@ -1,20 +1,7 @@
-#include <Arduino.h>
-
-#define led PA5
-
-#define s0 PB0
-#define s1 PB1
-#define s2 PB2
-#define s3 PB10
-
-#define mux1 PA0
-#define mux2 PA1
-#define lightgate PA4
-
-#define solenoid PA7
-
-#define TEENSYSERIAL Serial1
-#define DEBUG Serial2
+#include <main.h>
+#include <lightGate.cpp>
+#include <lightRing.cpp>
+#include <solenoid.cpp>
 
 void setup() {
   pinMode(led, OUTPUT);
@@ -31,17 +18,15 @@ void setup() {
 
   pinMode(solenoid, OUTPUT);
 
-  TEENSYSERIAL.begin(9600);
-
-  DEBUG.begin(9600);
-  DEBUG.println("Initialising...");
+  TEENSY.begin(9600);
 }
 
 void loop() {
-  // DEBUG.println(analogRead(lightgate));
-  digitalWrite(s0, LOW);
-  digitalWrite(s1, LOW);
-  digitalWrite(s2, LOW);
-  digitalWrite(s3, LOW);
-  DEBUG.println(analogRead(mux1));
+  detectLight();
+  processLight();
+  // TEENSY.println(analogRead(lightgate));
+  // digitalWrite(solenoid, HIGH);
+  // delay(5000);
+  // digitalWrite(solenoid, LOW);
+  // delay(5000);
 }
---
+++ programming/stm32a/src/main.h
@@ -0,0 +1,24 @@
+#ifndef main.h
+#define main.h
+
+#define botA
+
+#include <Arduino.h>
+
+#define led PA5
+
+#define s0 PB0
+#define s1 PB1
+#define s2 PB2
+#define s3 PB10
+
+#define mux1 PA0
+#define mux2 PA1
+#define lightgate PA4
+
+#define solenoid PA7
+
+#define TEENSY Serial1
+#define DEBUG Serial2
+
+#endif
